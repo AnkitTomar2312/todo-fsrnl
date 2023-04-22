323e501909d226c90bb741d7ef5f8527f44fb889
Ankit Tomar <ankitjdec@gmail.com>
2023-04-22 22:25:05 +0530

    completed the todofsrnl

--- src/Components/Task/Tasks.js
+++ src/Components/Task/Tasks.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "../Card/Card";
+import "./Tasks.css";
+import TaskList from "./TaskList";
+import TaskFilter from "./TaskFilter";
 
-export default function Tasks({ tasks }) {
+const Tasks = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredTasks = props.tasks.filter((task) => {
+    return task.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
-      <h1>Tasks</h1>
-      {tasks.map((item, index) => {
-        return (
-          <div key={index}>
-            <h1>{item.title}</h1>
-            <p>{item.status}</p>
-          </div>
-        );
-      })}
+      <Card className="tasks">
+        <TaskFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <TaskList items={filteredTasks} />
+      </Card>
     </div>
   );
-}
+};
+
+export default Tasks;
